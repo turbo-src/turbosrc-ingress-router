b29fc1ae046379d50d61720c9eaf55243fd4ec65
7db9a <>
2023-07-08 01:07:51 -0400

    Saving progress.

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser');
 const http = require('http');
 
 // The URI for the outGoingRouter
-const outGoingRouterUri = 'http://localhost:4006';
+const outGoingRouterUri = 'http://turbosrc-egress-router:4006';
 
-// Connect to the outGoingRouter
+// The URI for the turbosrc-service
+const turbosrcServiceUri = 'http://turbosrc-service:4000/graphql';
+
+// Connect to the outGoingRouter (aka egress-router)
 const socket = socketIO(outGoingRouterUri, {
   autoConnect: true
 });
@@ -18,23 +21,22 @@ const app = express();
 // Use JSON body parser for GraphQL
 app.use(bodyParser.json());
 
-// Route for GraphQL requests
-app.post('/graphql', (req, res) => {
-  // Create a unique ID for this request, could be a random UUID
-  const requestId = Date.now().toString();
+// Listen for messages from outGoingRouter via Socket.IO
+socket.on('graphqlRequest', async ({ requestId, query, variables }) => {
+  console.log("send-query:", query)
+  try {
+    // Send the request to turbosrc-service via HTTP
+    const res = await superagent
+      .post(turbosrcServiceUri)
+      .send({ query, variables })
+      .set('accept', 'json');
 
-  // Send GraphQL request to the outGoingRouter via socket
-  socket.emit('graphqlRequest', {
-    requestId: requestId,
-    query: req.body.query,
-    variables: req.body.variables
-  });
-
-  // Setup a one-time event listener for the response
-  socket.once(`graphqlResponse:${requestId}`, ({ data, errors }) => {
-    // Forward the response to the client
-    res.json({ data, errors });
-  });
+    // Forward the response back to outGoingRouter
+    socket.emit(`graphqlResponse:${requestId}`, res.body);
+  } catch (error) {
+    // Forward the error back to outGoingRouter
+    socket.emit(`graphqlResponse:${requestId}`, { errors: [{ message: error.message }] });
+  }
 });
 
 // Start HTTP server
